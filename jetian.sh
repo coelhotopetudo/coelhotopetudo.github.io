c81bd18bb7548998cdf57a885a9c4097c1b42f19
coelhotopetudo <coelhotopetudo@gmail.com>
2016-11-01 23:10:37 -0200

    Update jetian.sh

--- jetian.sh
+++ jetian.sh
@@ -1,3 +1,1 @@
-apt-get install git cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev
-apt-get install libboost-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev
-apt-get install mysql-server p7zip
+apt-get install -y git cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev libboost-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev mysql-server p7zip
