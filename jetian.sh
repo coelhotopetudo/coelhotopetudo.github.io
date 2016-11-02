adf221e81ba78b08c0f3cd10d6a23f71ab4ca06c
coelhotopetudo <coelhotopetudo@gmail.com>
2016-11-01 23:03:58 -0200

    Create jetian.sh

---
+++ jetian.sh
@@ -0,0 +1,3 @@
+apt-get install git cmake make gcc g++ libmysqlclient-dev libssl-dev libbz2-dev libreadline-dev libncurses-dev
+apt-get install libboost-dev libboost-thread-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev
+apt-get install mysql-server p7zip
