70637fce62bb35b2fa58cb9661273c10c694b663
Alisson Coelho de Morais <coelhotopetudo@gmail.com>
2016-11-29 13:35:36 -0200

    Exemplo mais complexo para descrobrir qual seria o horário ideal para saída, uma vez que tenho minutos em haver.

--- static/horario.py
+++ static/horario.py
@@ -6,22 +6,8 @@ def iatar(h, m):
 def datar(str):
   return datetime.strptime(str, '%H:%M')
 
-prent = datar('08:25')
-prsai = datar('13:33')
-esprsai = prent + timedelta(minutes=180)
-seent = datar('13:23')
-sesai = datar('17:00')
-exent = datar('17:00')
-exsai = datar('18:45')
-esexent = exsai - timedelta(minutes=120)
-essesai = seent + timedelta(minutes=180)
-essesai = esexent
-esseent = essesai - timedelta(minutes=180)
-esexsai = exent
-print prent
-print 'prsai: %s - %s' % (prsai, esprsai)
-print 'seent: ' + str(seent) + ' - ' + str(esseent)
-print 'sesai: %s - %s' % (sesai, essesai)
-print 'exent: %s - %s' % (exent, esexent)
-print 'exsai: %s - %s' % (exsai, esexsai)
-print (iatar(12, 1) - iatar(8 , 18)) + (iatar(16,38) - iatar(13,16))
+prent = datar('07:50')
+prsai = datar('11:55')
+seent = datar('12:55')
+print seent + ( timedelta(minutes=480) - (prsai - prent) - timedelta(minutes=35) )
+#esexent = exsai - timedelta(minutes=120)
