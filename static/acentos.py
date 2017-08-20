91526d370857f49f618fcb5869401d590d9c750a
Alisson COELHO de Morais <coelhotopetudo@gmail.com>
2017-08-19 23:36:26 -0300

    colocando front matter

--- static/acentos.py
+++ static/acentos.py
@@ -9,11 +9,29 @@ def remover_acentos(txt, codif='utf-8'):
 with open('/home/alisson/Downloads/presentation_type.csv') as f:
   spamreader = csv.reader(f, delimiter=',', quotechar='"')
   for line in spamreader:
+    desa = remover_acentos(line[1])
+    cabe = '-------------------------------\n'
+    cabe += 'title: ' + line[1] + '\n'
+    cabe += 'image: ' + desa + '\n'
+    cabe += '------------------------------\n'
+    cabe += '![' + line[1] + ']({{ site.baseurl }}/images/{{ page.image }}.jpg)\n'
+    cabe += '\n\n'
+    arq = open('/tmp/' + desa + '.md', 'w')
+    arq.write(cabe)
+    arq.close()
+
+with open('/home/alisson/Downloads/presentation_type.csv') as f:
+  spamreader = csv.reader(f, delimiter=',', quotechar='"')
+  for line in spamreader:
+    print line[2]
     cont = '# ' + line[3] + ': ' + line[2]
     cont += '\n\n'
     cont += line[6]
     cont += '\n\n'
-    cont += 'curriculo: ' + line[5]
+    cont += '## Currículo\n'
+    cont += line[5]
+    cont += '\n\n'
+    cont += '[Clique aqui para ver na grade](http://sistema.ftsl.org.br/ftsl9/grade/detail.html?pid=' + line[0] + ')'
     cont += '\n\n'
     desa = remover_acentos(line[1])
     open('/tmp/' + desa + '.md', 'a').write(cont)
