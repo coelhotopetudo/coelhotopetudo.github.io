ee973fca7a10be71b43856ebdcb2926d59778d63
Alisson COELHO de Morais <coelhotopetudo@gmail.com>
2017-08-19 21:49:59 -0300

    tah indo

--- static/acentos.py
+++ static/acentos.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from unicodedata import normalize
+import csv
 
 def remover_acentos(txt, codif='utf-8'):
   return normalize('NFKD', txt.decode(codif)).encode('ASCII','ignore').replace(' ', '').lower()
 
-print remover_acentos('[ArENTUAÇÃO] ç: áàãâä! éèêë? íì&#297;îï, óòõôö; úù&#361;ûü.')
+with open('/home/alisson/Downloads/presentation_type.csv') as f:
+  spamreader = csv.reader(f, delimiter=',', quotechar='"')
+  for line in spamreader:
+    cont = '# ' + line[3] + ': ' + line[2]
+    cont += '\n\n'
+    cont += line[6]
+    cont += '\n\n'
+    cont += 'curriculo: ' + line[5]
+    cont += '\n\n'
+    desa = remover_acentos(line[1])
+    open('/tmp/' + desa + '.md', 'a').write(cont)
+
+res = 'fim'
+print res
